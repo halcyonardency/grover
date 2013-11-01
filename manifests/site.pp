edc7a7b65c16f09557e8f1420374bc94517e6a34
Mike M <xyntrix@gmail.com>
2013-11-01 17:39:36 +0000

    o added firewall module

--- manifests/site.pp
+++ manifests/site.pp
@@ -4,8 +4,12 @@ Service { provider => systemd }
 node default {
    include vim
    include screen
-   #include denyhosts
+   include denyhosts
    include git
+   resources { "firewall":
+      purge => true
+   }
+   class { 'firewall': }
 #   class { 'sensu':
 #     rabbitmq_password  => 'secret',
 #     rabbitmq_host      => 'sensu-server.foo.com',
